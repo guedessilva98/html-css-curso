8a7046a2c7ed17d9ed75556f6a5fa573841fe9d1
André Guedes <guedessilva98@gmail.com>
2022-09-28 00:42:23 -0400

    Css e Javascript

---
+++ aula1/src/javascript/index.js
@@ -0,0 +1,9 @@
+var nome = "André"
+var notaDoPrimeiroBimestre = 9
+var notaDoSegundoBimestre = 8
+var notaDoTerceiroBimestre = 7
+var notaDoQuartoBimestre = 6
+var notaFinal = (notaDoPrimeiroBimestre + notaDoSegundoBimestre + notaDoTerceiroBimestre + notaDoQuartoBimestre)/4
+var notaFixada = notaFinal.toFixed(2)
+console.log("Olá " + nome)
+console.log(notaFixada)
