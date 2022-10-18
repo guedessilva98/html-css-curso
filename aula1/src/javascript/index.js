0cf1bd6b53e4afa5b6d2a7dd35d4cbab916e7623
André Guedes <guedessilva98@gmail.com>
2022-10-18 10:48:52 -0400

    Aula 1 completa

--- aula1/src/javascript/index.js
+++ aula1/src/javascript/index.js
@@ -1,9 +1,6 @@
-var nome = "André"
-var notaDoPrimeiroBimestre = 9
-var notaDoSegundoBimestre = 8
-var notaDoTerceiroBimestre = 7
-var notaDoQuartoBimestre = 6
-var notaFinal = (notaDoPrimeiroBimestre + notaDoSegundoBimestre + notaDoTerceiroBimestre + notaDoQuartoBimestre)/4
-var notaFixada = notaFinal.toFixed(2)
-console.log("Olá " + nome)
-console.log(notaFixada)
+function Converter() {
+    var valorElemento = document.getElementById("valor")
+    var valor = valorElemento.value
+    var valorEmDolarNumerico = parseFloat(valor)
+    console.log(valorEmDolarNumerico)
+}
